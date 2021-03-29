614bef0d5337e8391eb09e4703956137cd265f04
andre <andre.t.marques@tecnico.ulisboa.pt>
2021-03-29 18:32:45 +0100

    location badly implemented

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
@@ -1,21 +1,35 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
     ExtendedFloatingActionButton createButton;
     FloatingActionButton addButton;
 
-    private FusedLocationProviderClient fusedLocationClient;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9002;
+    double globalLatitude, globalLongitude;
 
 
     @Override
@@ -44,61 +61,97 @@ public class MainActivity extends AppCompatActivity {
         rotateClose = AnimationUtils.loadAnimation(this, R.anim.rotate_close);
         rotateOpen = AnimationUtils.loadAnimation(this, R.anim.rotate_open);
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getCurrentLocation();
-
-    }
-
-    public FusedLocationProviderClient getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                // Logic to handle location object
-                                return;
-                            }
-                        }
-                    });
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            getLocationPermission();
         }
-        return fusedLocationClient;
+        getLocationPermission();
+
+
     }
 
-
-
-    public void onClickButton(View v){
+    public void onClickButton(View v) {
         setVisibility(clicked);
         setAnimation(clicked);
         clicked = !clicked;
     }
 
-    public void setVisibility(Boolean clicked){
-        if(!clicked) {
+    public void setVisibility(Boolean clicked) {
+        if (!clicked) {
             joinButton.setVisibility(View.VISIBLE);
             createButton.setVisibility(View.VISIBLE);
-        }
-        else{
+        } else {
             joinButton.setVisibility(View.INVISIBLE);
             createButton.setVisibility(View.INVISIBLE);
 
         }
     }
 
-    public void setAnimation(Boolean clicked){
-        if(!clicked){
+    public void setAnimation(Boolean clicked) {
+        if (!clicked) {
             addButton.startAnimation(rotateOpen);
             joinButton.startAnimation(fromBottom);
             createButton.startAnimation(fromBottom);
-        }
-        else {
+        } else {
             joinButton.startAnimation(toBottom);
             createButton.startAnimation(toBottom);
             addButton.startAnimation(rotateClose);
+        }
+    }
+
+    public void getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    if(task.isSuccessful()) {
+                        Location location = task.getResult();
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        globalLatitude = latLng.latitude;
+                        globalLongitude = latLng.longitude;
+                        Log.d("INFO", "onComplete: latitude " + globalLatitude);
+                        Log.d("INFO", "onComplete: longitude " + globalLongitude);
+                    }
+
+                }
+            });
+            getRoadName();
+        }
+
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            getLastKnownLocation();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    public void getRoadName() {
+        String myCity = "";
+        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(globalLatitude, globalLongitude,1);
+            String address = addresses.get(0).getAddressLine(0);
+            myCity = addresses.get(0).getAddressLine(0);
+            Log.d("TAG", "Address: " + address);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
