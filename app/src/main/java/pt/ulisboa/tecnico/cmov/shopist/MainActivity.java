75ba730eb8f84d058b5b236cf25b2679586b8f8d
ist190625 <ist190625@tecnico.ulisboa.pt>
2021-04-09 21:18:51 +0100

    NOT WORKING

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/MainActivity.java
@@ -1,12 +1,15 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -14,6 +17,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,17 +25,23 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.common.internal.Constants;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.schibstedspain.leku.LocationPickerActivity;
+
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private Animation fromBottom;
     private Animation toBottom;
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Places.initialize(getApplicationContext(),getString(R.string.key_google_apis_android));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -70,27 +81,27 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-        LocationListener LocationListenerGPS = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.d("OLA", "onLocationChanged: " + location);
-                String address = getRoad(location.getLatitude(), location.getLongitude());
-                GPStext.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_location_on_24, 0, 0, 0);
-                GPStext.setText(address);
-            }
+    LocationListener LocationListenerGPS = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("OLA", "onLocationChanged: " + location);
+            String address = getRoad(location.getLatitude(), location.getLongitude());
+            GPStext.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_location_on_24, 0, 0, 0);
+            GPStext.setText(address);
+        }
 
-            @Override
-            public void onProviderDisabled(String provider) {
-                Log.d("Latitude", "disable");
-            }
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("Latitude", "disable");
+        }
 
-            @Override
-            public void onProviderEnabled(String provider) {
-                Log.d("Latitude", "enable");
-            }
-        };
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d("Latitude", "enable");
+        }
+    };
 
-    public void showCreatePopUp (View v){
+    public void showCreatePopUp(View v) {
         final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this, R.style.BottomSheetDialogTheme);
         View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.new_list_layout, (LinearLayout) findViewById(R.id.newListContainer));
         bottomSheetView.findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
@@ -101,7 +112,63 @@ public class MainActivity extends AppCompatActivity{
         });
         bottomSheetDialog.setContentView(bottomSheetView);
         bottomSheetDialog.show();
+
+        bottomSheetView.findViewById(R.id.pick_location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPlacePicker();
+            }
+        });
     }
+
+    private void showPlacePicker() {
+
+        Intent locationPickerIntent = new LocationPickerActivity.Builder()
+                .withLocation(41.4036299, 2.1743558)
+                .withGeolocApiKey(getString(R.string.key_google_apis_android))
+                .withSearchZone("es_ES")
+                .withDefaultLocaleSearchZone()
+                .shouldReturnOkOnBackPressed()
+                .withStreetHidden()
+                .withCityHidden()
+                .withZipCodeHidden()
+                .withSatelliteViewHidden()
+                .withGoogleTimeZoneEnabled()
+                .withVoiceSearchHidden()
+                .withUnnamedRoadHidden()
+                .build(this.getApplicationContext());
+        try {
+            startActivityForResult(locationPickerIntent, 1);
+
+        } catch (Exception ex) {
+            Log.d("FAIL_BRO", "showPlacePicker: RIP");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            Log.d("RESULT****", "OK");
+            if (requestCode == 1) {
+                double latitude = data.getDoubleExtra("LATITUDE", 0.0);
+                Log.d("LATITUDE****", "lat" + latitude);
+                double longitude = data.getDoubleExtra("LONGITUDE", 0.0);
+                Log.d("LONGITUDE****", "" + longitude);
+            } else if (requestCode == 2) {
+                double latitude = data.getDoubleExtra("LATITUDE", 0.0);
+                Log.d("LATITUDE****", "lat" + latitude);
+                double longitude = data.getDoubleExtra("LONGITUDE", 0.0);
+                Log.d("LONGITUDE****", "" + longitude);
+                String address = data.getStringExtra("LOCATION_ADDRESS");
+                Log.d("ADDRESS****", address);
+            }
+        }
+        if (resultCode == Activity.RESULT_CANCELED) {
+            super.onActivityResult(requestCode, resultCode, data);
+            Log.d("RESULT****", "CANCELLED");
+        }
+    }
+
 
     public void onClickButton(View v) {
         setVisibility(clicked);
@@ -139,13 +206,10 @@ public class MainActivity extends AppCompatActivity{
     public String getRoad(double latitude, double longitude) {
         String address = "";
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        Log.d("INFO", "road: latitude " + latitude);
-        Log.d("INFO", "road: longitude " + longitude);
 
         try {
             List<Address> addresses = geocoder.getFromLocation(latitude, longitude,1);
             address = addresses.get(0).getAddressLine(0);
-            Log.d("TAG", "Address: " + address);
 
         } catch (IOException e) {
             e.printStackTrace();
