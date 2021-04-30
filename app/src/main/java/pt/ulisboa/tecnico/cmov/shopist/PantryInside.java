60404ea99b24348c12d3f39526abca546a7fbb8e
andre <andre.t.marques@tecnico.ulsiboa.pt>
2021-04-30 01:12:28 +0100

    fix comms

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/PantryInside.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/PantryInside.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.OnBackPressedCallback;
@@ -17,10 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class PantryInside extends AppCompatActivity {
     private ArrayList<Item> itemsPantry = new ArrayList<>();
--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/PriceShopActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/PriceShopActivity.java
@@ -2,7 +2,6 @@ package pt.ulisboa.tecnico.cmov.shopist;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
