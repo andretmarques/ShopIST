442cc7d7f48d98571f2e816fdd7c7695aa83eb3f
André Marques <andre.t.marques@tecnico.ulisboa.pt>
2021-05-04 19:14:47 +0100

    registering new accounts now work

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/RegisterUser.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/RegisterUser.java
@@ -1,5 +1,6 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,8 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 
 public class RegisterUser extends AppCompatActivity {
@@ -19,33 +31,39 @@ public class RegisterUser extends AppCompatActivity {
     private EditText emailEditText, passwordEditText;
     private ProgressBar progressBar;
     private Button registerBtn;
+    private DatabaseReference myRef;
 
-
+    String email;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://shopist-310217-default-rtdb.europe-west1.firebasedatabase.app/");
+        myRef = database.getReference();
         mAuth = FirebaseAuth.getInstance();
 
         banner = findViewById(R.id.banner);
         emailEditText = findViewById(R.id.email_register);
         passwordEditText = findViewById(R.id.password_register);
-        progressBar = findViewById(R.id.loading);
+        progressBar = findViewById(R.id.progress_bar);
         registerBtn = findViewById(R.id.register_btn);
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                registerUser();
+                if (registerUser()) {
+                    newUser();
+                }
             }
         });
     }
 
-    private void registerUser() {
-        String email = emailEditText.getText().toString().trim();
-        String password = passwordEditText.getText().toString().trim();
+    private Boolean registerUser() {
+        email = emailEditText.getText().toString().trim();
+        password = passwordEditText.getText().toString().trim();
         if (password.length() < 6) {
             if (password.isEmpty()) {
                 passwordEditText.setError("Empty");
@@ -53,7 +71,6 @@ public class RegisterUser extends AppCompatActivity {
                 passwordEditText.setError("Minimum password length is 6 characters");
             }
             passwordEditText.requestFocus();
-
         }
 
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
@@ -63,8 +80,39 @@ public class RegisterUser extends AppCompatActivity {
                 emailEditText.setError("Provide a valid email");
             }
             emailEditText.requestFocus();
+            return false;
         }
-
-
+        else {
+            return true;
+        }
     }
+
+    public void newUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    User user = new User(email);
+
+                    myRef.child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterUser.this, "User successfully registered", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(RegisterUser.this, "Failed to register. Try again", Toast.LENGTH_LONG).show();
+                            }
+                            progressBar.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegisterUser.this, "Failed to register. Try again", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/User.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/User.java
@@ -1,4 +1,18 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
 public class User {
+
+    public String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String email) {
+        this.email = email;
+    }
 }
