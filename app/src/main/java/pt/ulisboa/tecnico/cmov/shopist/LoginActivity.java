6fd16ad1183f57c08d50dfa9ec32aac3c1ef8345
André Marques <andre.t.marques@tecnico.ulisboa.pt>
2021-05-04 19:35:59 +0100

    login is now possible

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/LoginActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/LoginActivity.java
@@ -1,27 +1,23 @@
 package pt.ulisboa.tecnico.cmov.shopist;
 
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.util.Patterns;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     Button loginButton;
     ProgressBar loadingProgressBar;
 
+    FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,7 +39,53 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.sign_in);
         loadingProgressBar = findViewById(R.id.loading);
         register = findViewById(R.id.register);
+
+        mAuth = FirebaseAuth.getInstance();
+
         register.setOnClickListener(view -> startActivityForResult(new Intent(LoginActivity.this, RegisterUser.class), 12119));
+        loginButton.setOnClickListener(view -> userLogin());
+    }
+
+    private void userLogin() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (password.length() < 6) {
+            if (password.isEmpty()) {
+                passwordEditText.setError("Empty");
+            } else {
+                passwordEditText.setError("Minimum password length is 6 characters");
+            }
+            passwordEditText.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (email.isEmpty()) {
+                emailEditText.setError("Empty");
+            } else {
+                emailEditText.setError("Provide a valid email");
+            }
+            emailEditText.requestFocus();
+            return;
+        }
+
+        loadingProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Failed to Login. Check your credentials", Toast.LENGTH_LONG).show();
+                    emailEditText.setText(null);
+                    passwordEditText.setText(null);
+                    loadingProgressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
 }
